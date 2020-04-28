8af645b06b52112cfc6b12f3239ba486ff100b34
Kirill_Boreiko <kirill_boreiko@epam.com>
2020-04-28 10:48:37 +0600

    Added unit test to task 3

---
+++ Module2/Task3 (Exceptions)/ParserClassLibrary/UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("46f3fa87-7dd9-41f3-bcca-e50730274b0f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Module2/Task3 (Exceptions)/ParserClassLibrary/UnitTest/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using ParserClassLibrary;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class ParserTest
+    {
+        IntParser parser;
+
+        public ParserTest()
+        {
+            SetUp();
+        }
+        [SetUp]
+        public void SetUp()
+        {
+            parser = new IntParser();
+        }
+        [TestMethod]
+        public void ParseStringToInt_ReturnInt()
+        {          
+            string s = "104";
+         
+            var actualResult = parser.Parse(s);
+
+            Assert.AreEqual(actualResult, 104);
+        }
+
+
+        [TestMethod]
+        public void ParseStringToInt_ReturnZero()
+        {
+            string s = "1MSD0";
+
+            var actualResult = parser.Parse(s);
+
+            Assert.AreEqual(actualResult, 0);
+          
+        //    Assert.ThrowsException<Exception>(() => parser.Parse(s));                      
+        }
+
+
+        [TestMethod]
+        public void ParseEmptyStringToInt_ReturnZero()
+        {
+            string s = "";
+
+            var actualResult = parser.Parse(s);
+
+            Assert.AreEqual(actualResult, 0);                              
+        }
+
+    }
+}
