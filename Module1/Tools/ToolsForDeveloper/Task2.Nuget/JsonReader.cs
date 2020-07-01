7bca6850ce3826a1e3928a57cb625eaf782c985a
Kirill_Boreiko <kirill_boreiko@epam.com>
2020-07-01 23:53:04 +0600

    Added nuget packages task

---
+++ Module1/Tools/ToolsForDeveloper/Task2.Nuget/JsonReader.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tools
+{
+    public class jsonReader
+    {
+        public object ReadJson(List<string> Json)
+        {
+            return JsonConvert.DeserializeObject(Json.First());
+        }
+    }
+}
---
+++ Module1/Tools/ToolsForDeveloper/Task2Test/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tools;
+
+namespace Task2Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            jsonReader jr = new jsonReader();
+
+
+            string json = @"{
+            'Email': 'james@example.com',
+             'Active': true,
+             'CreatedDate': '2013-01-20T00:00:00Z',
+            }";
+
+            string json2 = @"{
+            'Email': 'james@example.com',
+             'Active': true,
+             'CreatedDate': '2013-01-20T00:00:00Z',
+            }";
+            List<string> mylist = new List<string>(new string[] { json, json2 });
+
+
+            object responce = jr.ReadJson(mylist);
+            Console.WriteLine(responce);
+            Console.ReadLine();
+        }
+    }
+}
