158cb82bba5e28587052e936844f7536aed4ff9b
Kirill_Boreiko <kirill_boreiko@epam.com>
2020-09-21 23:18:35 +0600

    Added serialization task 1

---
+++ Module12 (Serialization)/SerializationSolutionTask1/Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using Task1.Models;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Catalog));
+            Catalog catalog = new Catalog();
+
+            using (var fileStream = File.OpenRead("..\\..\\books.xml"))
+            {
+                catalog = (Catalog)serializer.Deserialize(fileStream);
+            }
+       
+            using (var xmlWriter = XmlWriter.Create("..\\..\\books1.xml", new XmlWriterSettings
+            {
+                Encoding = Encoding.UTF8,
+                Indent = true
+            }))
+            {
+                serializer.Serialize(xmlWriter, catalog);
+            }
+        }
+    }
+}
